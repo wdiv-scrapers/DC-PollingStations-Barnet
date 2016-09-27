886ddfa8c95d8dc437e605296f831ff7fe7e467d
chris48s <c.mshaw@yahoo.co.uk>
2016-09-27 20:10:40 +0100

    init

---
+++ scraper.py
@@ -0,0 +1,20 @@
+from dc_base_scrapers.ckan_scraper import CkanScraper
+
+base_url = 'https://open.barnet.gov.uk/api/3/action/package_show?id='
+
+stations_info = {
+    'dataset': 'polling-stations-in-barnet',
+    'extra_fields': [],
+    'return_format': 'CSV',
+}
+
+council_id = 'E09000003'
+
+
+stations_meta_scraper = CkanScraper(
+    base_url,
+    stations_info['dataset'],
+    stations_info['return_format'],
+    stations_info['extra_fields'],
+    'utf-8')
+stations_url = stations_meta_scraper.scrape()
